1588993fafb27b76189bdbdd39ab2490fd0f04b0
rolandzagonyi@gmail.com <rolandzagonyi@gmail.com>
2018-08-03 21:25:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {'Phil Ochs': 'Here\'s to the State of Mississippi'}
 
 function updatePlaylist (playlist, artistName, songTitle)
@@ -9,5 +10,20 @@ function updatePlaylist (playlist, artistName, songTitle)
 function removeFromPlaylist (playlist, artistName)
 {
   delete playlist[artistName]
+=======
+var playlist = {}
+var artistName = {}
+var songTitle = {}
+
+function updatePlaylist (playlist, artistName, songTitle)
+{
+  playlist.artistName = songTitle
+  return playlist
+}
+
+function removePlaylist (playlist, artistName)
+{
+  delete playlist.artistName
+>>>>>>> 111c3d77d1ad8fd8f97e8ffd1758a047168d61f6
   return playlist
 }
